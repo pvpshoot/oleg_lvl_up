d5e435c3267b21f81ce5369ba91a471824dfb5c6
Олег Наянов <oleg.nayanov@dz.ru>
2018-03-17 12:38:40 +0400

    Исправлено: работа функции equals

--- functions/equals.js
+++ functions/equals.js
@@ -1,3 +1,9 @@
 export default (a, b) => {
-  return a === b;
+  if (typeof a === "string" && typeof b === "string") {
+    return a === b;
+  }
+  
+  if (Array.isArray(a) && Array.isArray(b)) {
+    return a.join() === b.join();
+  }
 };
