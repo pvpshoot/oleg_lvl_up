6065e79f021ad9c43786ff127409415d4d0762d7
Михаил Синяков <pvpshoot@gmail.com>
2018-03-17 13:36:40 +0400

    add new tests

--- test/equals.test.js
+++ test/equals.test.js
@@ -5,6 +5,18 @@ describe("equals сравнивает 2 аргумента", () => {
     expect(equals("Олег где макет", "Олег где макет")).toBeTruthy();
   });
 
+  test("два одинаковых числа", () => {
+    expect(equals({}, {})).toBeTruthy();
+    expect(equals({ a: 1 }, { a: 1 })).toBeTruthy();
+    expect(equals({ a: 1 }, { a: 2 })).toBeFalsy();
+    expect(equals({}, '1')).toBeFalsy();
+  });
+
+  test("два одинаковых числа", () => {
+    expect(equals(1, 1)).toBeTruthy();
+    expect(equals(1, '1')).toBeFalsy();
+  });
+
   test("два одинаковых массива", () => {
     expect(equals([], [])).toBeTruthy();
     expect(equals([1, 2, 3], [1, 2, 3])).toBeTruthy();
