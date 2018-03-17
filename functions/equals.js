9ba64ad8f71ce4d7519646753ddb0a7fa65a3557
Олег Наянов <oleg.nayanov@dz.ru>
2018-03-17 13:20:12 +0400

    Исправлено: проверка массивов в equals

--- functions/equals.js
+++ functions/equals.js
@@ -1,9 +1,22 @@
 export default (a, b) => {
+  if (typeof a !== typeof b) {
+    return false;
+  }
+
   if (typeof a === "string" && typeof b === "string") {
     return a === b;
   }
-  
+
   if (Array.isArray(a) && Array.isArray(b)) {
-    return a.join() === b.join();
+    if (a.length !== b.length) {
+      return false;
+    }
+
+    const result = a.reduce((acc, value, index)=> {
+      return value !== b[index] ? false : true;
+    }, true);
+
+    return result;
+
   }
 };
